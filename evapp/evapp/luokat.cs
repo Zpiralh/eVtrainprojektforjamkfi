e9e1e27d4801cd321fc3ad6f57aa4ed1c9c6103c
Niko Valkonen <k2543@student.jamk.fi>
2016-03-22 18:50:23 +0200

    aloitin rakentemaan Lippu luokkaa lähinnä kokeiluna.

---
+++ evapp/evapp/luokat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evapp
+{
+    class Lippu
+    {
+        public string petunimi;
+        public string psukunimi;
+        public string plahtoasema;
+        public string ppaateasema;
+    }
+}
