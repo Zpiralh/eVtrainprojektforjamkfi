29345ef404350a4920e7aafbbe637994d7a09d18
Panu Hakala <phakala@live.fi>
2016-04-25 04:14:11 +0300

    mainpage aloitettu, mutta yhtäkkiä database alhaalla. selvitellään

--- evapp/evapp/MainPage.xaml.cs
+++ evapp/evapp/MainPage.xaml.cs
@@ -22,14 +22,51 @@ namespace evapp
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary
     ///>
-
+    
     public sealed partial class MainPage : Page
     {
+        Dictionary<int, Junavuoro> vuorot = new Dictionary<int, Junavuoro>();
+        //public databaseMYSQL database = new databaseMYSQL("sql7.freemysqlhosting.net", 3306, "sql7116678", "H1Fwg1G2Hl", "sql7116678");
+
         public MainPage()
         {
             this.InitializeComponent();
-        }
+            /*vuorot.Clear();
+            grid1.Visibility = Visibility.Collapsed;
+            grid2.Visibility = Visibility.Collapsed;
+            grid3.Visibility = Visibility.Collapsed;
+            grid4.Visibility = Visibility.Collapsed;
+            grid5.Visibility = Visibility.Collapsed;*/
+            
 
+
+
+
+        }
+        /*public void NextTrains()
+        {
+
+            string aika = "'" + DateTime.Now.ToString("hh:mm:00") + "'";
+            //Grid[] gridit = { grid1, grid2, grid3, grid4, grid5 };
+            //TextBlock[] lahtoblokit = { lahtoBlock1, lahtoBlock2, lahtoBlock3, lahtoBlock4, lahtoBlock5 };
+            //TextBlock[] aikablokit = { aikaBlock1, aikaBlock2, aikaBlock3, aikaBlock4, aikaBlock5 };
+            string kekke = database.GetRoutes("SELECT * FROM Junavuoro WHERE Lahtoaika > " + aika + " ORDER BY Lahtoaika LIMIT 5;", ref vuorot);
+            if (kekke == "OK")
+            {
+                int krt = 0;
+                foreach (Junavuoro vuoro in vuorot.Values)
+                {
+                    //gridit[krt].Visibility = Visibility.Visible;
+                    //asemablokit[krt].Text = vuoro.Lahtoasema;
+                    krt++;
+                }
+            }
+            else
+            {
+                //grid1.Visibility = Visibility.Visible;
+            }
+            
+        }*/
         private void button_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(Search));
@@ -37,7 +74,9 @@ namespace evapp
 
         private void button_Click_1(object sender, RoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(Customerservice));
+            //this.Frame.Navigate(typeof(Customerservice));
+            //NextTrains();
         }
+
     }
 }
